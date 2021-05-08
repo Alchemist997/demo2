254ff7d59bdd74c1e9b7af1d2d8657afc2d6236a
Alchemist997 <brainbrain160@gmail.com>
2021-05-08 17:02:56 +0300

    1

--- arrow_rotator.js
+++ arrow_rotator.js
@@ -1,1 +1,32 @@
-window.addEventListener("DOMContentLoaded",(function(){let e=document.querySelectorAll("select"),t=document.querySelectorAll(".jqselect"),o=document.querySelectorAll(".jq-selectbox__trigger-arrow");for(let n=0;n<o.length;n++)t[n].onclick=function(){t[n].classList.contains("opened")?o[n].style.transform="translateY(4px) rotate(-135deg)":o[n].style.transform="rotate(45deg)"},e[n].onblur=function(){o[n].style.transform="rotate(45deg)"}}));
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/js/select_plugin/arrow_rotator.js":
+/*!***********************************************!*\
+  !*** ./src/js/select_plugin/arrow_rotator.js ***!
+  \***********************************************/
+/***/ (() => {
+
+eval("window.addEventListener('DOMContentLoaded', function() {\r\n\tlet selectsOrig = document.querySelectorAll('select');\r\n\tlet selects = document.querySelectorAll('.jqselect');\r\n\tlet arrows = document.querySelectorAll('.jq-selectbox__trigger-arrow');\r\n\r\n\tfor (let i = 0; i < arrows.length; i++) {\r\n\t\tselects[i].onclick = function () { // Разворот стрелки при кликах по селекту\r\n\t\t\tif (selects[i].classList.contains('opened')) {\r\n\t\t\t\tarrows[i].style.transform = 'translateY(4px) rotate(-135deg)';\r\n\t\t\t} else {\r\n\t\t\t\tarrows[i].style.transform = 'rotate(45deg)';\r\n\t\t\t}\r\n\t\t}\r\n\t\tselectsOrig[i].onblur = function () { // Возврат стрелки при клике вне селекта\r\n\t\t\tarrows[i].style.transform = 'rotate(45deg)';\r\n\t\t}\r\n\t}\r\n});\n\n//# sourceURL=webpack://tochka/./src/js/select_plugin/arrow_rotator.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/js/select_plugin/arrow_rotator.js"]();
+/******/ 	
+/******/ })()
+;
--- checkbox.js
+++ checkbox.js
@@ -1,1 +1,32 @@
-(()=>{let e=document.querySelector(".checkbox"),o=document.querySelector(".checkbox-area"),r=document.querySelector(".checkbox-area__mark"),l=document.querySelectorAll(".hiding"),c=function(){o.style.border="none",o.style.backgroundColor="#EF483E",r.style.display="block";for(let e=0;e<l.length;e++)l[e].style.display="block"};e.checked&&c(),e.onchange=function(){if(e.checked)c();else{o.style.removeProperty("border"),o.style.removeProperty("background-color"),r.style.removeProperty("display");for(let e=0;e<l.length;e++)l[e].style.display=""}}})();
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/js/select_plugin/checkbox.js":
+/*!******************************************!*\
+  !*** ./src/js/select_plugin/checkbox.js ***!
+  \******************************************/
+/***/ (() => {
+
+eval("let checkbox = document.querySelector('.checkbox');\r\nlet checkboxArea = document.querySelector('.checkbox-area');\r\nlet mark = document.querySelector('.checkbox-area__mark');\r\nlet hidings = document.querySelectorAll('.hiding');\r\n\r\nlet sign = function() {\r\n    checkboxArea.style.border = 'none';\r\n    checkboxArea.style.backgroundColor = '#EF483E';\r\n    mark.style.display = 'block';\r\n    for (let i = 0; i < hidings.length; i++) {\r\n        hidings[i].style.display = 'block';\r\n    };\r\n};\r\n\r\nif (checkbox.checked) {\r\n    sign();\r\n};\r\n\r\ncheckbox.onchange = function() {\r\n    if (checkbox.checked) {\r\n        sign();\r\n    } else {\r\n        checkboxArea.style.removeProperty('border');\r\n        checkboxArea.style.removeProperty('background-color');\r\n        mark.style.removeProperty('display');\r\n        for (let i = 0; i < hidings.length; i++) {\r\n            hidings[i].style.display = '';\r\n        };\r\n    };\r\n};\n\n//# sourceURL=webpack://tochka/./src/js/select_plugin/checkbox.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/js/select_plugin/checkbox.js"]();
+/******/ 	
+/******/ })()
+;
