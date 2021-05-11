fc3117073948a83807d5d238973b66caf3952133
Alchemist997 <brainbrain160@gmail.com>
2021-05-11 16:06:15 +0300

    Added more browsers

--- agent.js
+++ agent.js
@@ -1,24 +1,65 @@
-let p = document.querySelectorAll('p');
+let pm = document.querySelector('.mw p');
+let type = document.querySelectorAll('.type p');
+let data = document.querySelectorAll('.data p');
 
 //alert(window.navigator.userAgent)
 
-let isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
-if (!isSafari) {
-    console.log('Is not a Safari');
-    p[0].textContent = 'Is not a Safari';
+pm.textContent = window.navigator.userAgent;
+
+let isSafari = /^((?!chrome|android|edg).)*safari/i.test(navigator.userAgent);
+if (isSafari) {
+    data[0].textContent = 'Is Safari';
 } else {
-    console.log('Is Safari');
-    p[0].textContent = 'Is Safari';
+    data[0].textContent = 'Is not a Safari';
 }
 
-let safariAgent = navigator.userAgent.indexOf("Safari") > -1;
-//if ((chromeAgent) && (safariAgent)) safariAgent = false;
-if (!safariAgent) {
-    console.log('Is not a Safari');
-    p[1].textContent = 'Is not a Safari';
-} else {
-    console.log('Is Safari ?!');
-    p[1].textContent = 'Is Safari ?!';
-}
+// Get the user-agent string
+let usAgStr = navigator.userAgent;
 
-p[2].textContent = window.navigator.userAgent;
+// Detect Chrome
+let chromeAgent =
+usAgStr.indexOf("Chrome") > -1;
+
+// Detect Firefox
+let firefoxAgent =
+usAgStr.indexOf("Firefox") > -1;
+
+// Detect Edge
+let edgeAgent =
+usAgStr.indexOf("Edg") > -1;
+
+// Detect Internet Explorer
+let ieAgent =
+usAgStr.indexOf("MSIE") > -1 ||
+usAgStr.indexOf("rv:") > -1;
+// Discard IE in Firefox
+if ((ieAgent) && (firefoxAgent))
+ieAgent = false;
+
+// Detect Safari
+let safariAgent =
+usAgStr.indexOf("Safari") > -1;
+// Discard Safari in Chrome or Edge
+if ((safariAgent) && (chromeAgent) || (edgeAgent))
+safariAgent = false;
+
+// Detect Opera
+let operaAgent =
+usAgStr.indexOf("OP") > -1;
+// Discard Chrome in Opera or Edge
+if ((chromeAgent) && (operaAgent) || (edgeAgent))
+chromeAgent = false;
+
+data[1].textContent = safariAgent;
+data[2].textContent = chromeAgent;
+data[3].textContent = firefoxAgent;
+data[4].textContent = ieAgent;
+data[5].textContent = operaAgent;
+data[6].textContent = edgeAgent;
+
+for (let i = 0; i < data.length; i++) {
+    if (data[i].textContent == 'true') {
+        data[i].style.color = '#00ff00';
+        type[i].style.color = '#f7f7f7';
+    }
+}
